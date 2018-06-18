c8ad74e7daefd1c7a408086ff3ec83cd55508fdf
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-18 15:26:03 +0200

    Things

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,22 @@
 const webpack = require('webpack');
 const path = require('path');
-// React v.16 uses some newer JS functionality, so to ensure everything
-// works across all browsers, we're adding babel-polyfill here.
-require('babel-polyfill');
-
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
 module.exports = {
-  entry: [
-    './src/index'
-  ],
-  module: {
-    loaders: [
-      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
-    ]
-  },
-  resolve: {
-    modules: [
-      path.resolve('./'),
-      path.resolve('./node_modules'),
-    ],
-    extensions: ['.js','.scss'],
-  },
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'dist.bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'cheap-eval-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
+  module: {
+    rules: []
   },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
+  plugins: {
+    minimize: true
+  }
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,13 +1,4 @@
 const config = require('./webpack.config.js');
 const webpack = require('webpack');
 
-config.plugins.push(
-  new webpack.DefinePlugin({
-    "process.env": {
-      "NODE_ENV": JSON.stringify("production")
-    }
-  })
-);
-config.plugins.minimize = true;
-
 module.exports = config;
